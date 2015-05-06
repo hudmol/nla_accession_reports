88d273edd53672c02e85e602de0433a70841ed42
Payten Giles <payten.giles@gmail.com>
2015-05-06 20:27:23 +1000

    Redo report endpoints in the backend so the newly registered reports have backend API generated. Add some params to the Work Plan report.

--- backend/controllers/reports.rb
+++ backend/controllers/reports.rb
@@ -2,37 +2,50 @@ class ArchivesSpaceService < Sinatra::Base
 
   include ReportHelper::ResponseHelpers
 
-  Endpoint.get("/repositories/:repo_id/reports/nla_valuation_required")
-  .description("Report on accessions where valuations are required")
-  .params(ReportHelper.report_formats,
-          ["repo_id", :repo_id])
-  .permissions([])
-  .returns([200, "report"]) \
-      do
-    report_response(AccValuationRequiredReport.new(params), params[:format])
+  # Endpoint.get("/repositories/:repo_id/reports/nla_valuation_required")
+  # .description("Report on accessions where valuations are required")
+  # .params(ReportHelper.report_formats,
+  #         ["repo_id", :repo_id])
+  # .permissions([])
+  # .returns([200, "report"]) \
+  #     do
+  #   report_response(AccValuationRequiredReport.new(params), params[:format])
+  # end
+  #
+  #
+  # Endpoint.get("/repositories/:repo_id/reports/nla_valuation_completed")
+  # .description("Report on accessions where valuations are completed")
+  # .params(ReportHelper.report_formats,
+  #         ["repo_id", :repo_id])
+  # .permissions([])
+  # .returns([200, "report"]) \
+  #     do
+  #   report_response(AccValuationCompletedReport.new(params), params[:format])
+  # end
+  #
+  #
+  # Endpoint.get("/repositories/:repo_id/reports/nla_work_plan_development")
+  # .description("Work Plan Development report")
+  # .params(ReportHelper.report_formats,
+  #         ["repo_id", :repo_id])
+  # .permissions([])
+  # .returns([200, "report"]) \
+  #     do
+  #   report_response(WorkPlanDevelopmentReport.new(params), params[:format])
+  # end
+
+
+  ReportManager.registered_reports.each do |uri_suffix, opts|
+
+    Endpoint.get("/repositories/:repo_id/reports/#{uri_suffix}")
+    .description(opts[:description])
+    .params(*(opts[:params] << ReportHelper.report_formats << ["repo_id", :repo_id]))
+    .permissions([])
+    .returns([200, "report"]) \
+    do
+      report_response(opts[:model].new(params), params[:format])
+    end
+
   end
-
-
-  Endpoint.get("/repositories/:repo_id/reports/nla_valuation_completed")
-  .description("Report on accessions where valuations are completed")
-  .params(ReportHelper.report_formats,
-          ["repo_id", :repo_id])
-  .permissions([])
-  .returns([200, "report"]) \
-      do
-    report_response(AccValuationCompletedReport.new(params), params[:format])
-  end
-
-
-  Endpoint.get("/repositories/:repo_id/reports/nla_work_plan_development")
-  .description("Work Plan Development report")
-  .params(ReportHelper.report_formats,
-          ["repo_id", :repo_id])
-  .permissions([])
-  .returns([200, "report"]) \
-      do
-    report_response(WorkPlanDevelopmentReport.new(params), params[:format])
-  end
-
 
 end
--- backend/model/work_plan_development_report.rb
+++ backend/model/work_plan_development_report.rb
@@ -3,8 +3,29 @@ class WorkPlanDevelopmentReport < AbstractReport
   register_report({
                     :uri_suffix => "nla_work_plan_development",
                     :description => "Work Plan Development Report",
-
+                    :params => [
+                      ["processing_status", String, "Processing Status", {
+                        optional: true,
+                        validation: [
+                          "Must be one of #{BackendEnumSource.values_for("collection_management_processing_status").join(", ")}",
+                          ->(v){ v.nil? || v == "" || BackendEnumSource.valid?("collection_management_processing_status", v) }
+                        ]
+                      }],
+                      ["processing_priority", String, "Processing Priority", {
+                        optional: true,
+                        validation: [
+                          "Must be one of #{BackendEnumSource.values_for("collection_management_processing_priority").join(", ")}",
+                          ->(v){ v.nil? || v == "" || BackendEnumSource.valid?("collection_management_processing_priority", v) }
+                        ]
+                      }],
+                    ]
                   })
+
+  def initialize(params)
+    super
+    @processing_status = params[:processing_status] != "" ? params[:processing_status] : nil
+    @processing_priority = params[:processing_priority] != "" ?  params[:processing_priority] : nil
+  end
 
   def title
     "Accessions - Work Plan Development"
@@ -29,7 +50,7 @@ class WorkPlanDevelopmentReport < AbstractReport
       'Inventory' => proc{|record| record[:inventory]},
       'Acq Method' => proc{|record| I18n.t("enumerations.accession_acquisition_type.#{record[:acquisition_type]}", :default => record[:acquisition_type])},
       'Processing Plan' => proc{|record| record[:processing_plan]},
-      'Accessioning Priority' => proc{|record| record[:accessioning_priority]},
+      'Accessioning Priority' => proc{|record| record[:processing_priority]},
     }
   end
 
@@ -57,6 +78,20 @@ class WorkPlanDevelopmentReport < AbstractReport
            {
              :table_alias => :enum_extent_type
            }).
+      join(:enumeration,
+           {
+             :name => 'collection_management_processing_status'
+           },
+           {
+             :table_alias => :enum_processing_status
+           }).
+      join(:enumeration,
+           {
+             :name => 'collection_management_processing_priority'
+           },
+           {
+             :table_alias => :enum_processing_priority
+           }).
       left_outer_join(:enumeration_value,
                       {
                         Sequel.qualify(:enumvals_acquisition_type, :enumeration_id) =>  Sequel.qualify(:enum_acquisition_type, :id),
@@ -73,6 +108,22 @@ class WorkPlanDevelopmentReport < AbstractReport
                       {
                         :table_alias => :enumvals_extent_type
                       }).
+      left_outer_join(:enumeration_value,
+                      {
+                        Sequel.qualify(:enumvals_processing_status, :enumeration_id) =>  Sequel.qualify(:enum_processing_status, :id),
+                        Sequel.qualify(:collection_management, :processing_status_id) => Sequel.qualify(:enumvals_processing_status, :id),
+                      },
+                      {
+                        :table_alias => :enumvals_processing_status
+                      }).
+      left_outer_join(:enumeration_value,
+                      {
+                        Sequel.qualify(:enumvals_processing_priority, :enumeration_id) =>  Sequel.qualify(:enum_processing_priority, :id),
+                        Sequel.qualify(:collection_management, :processing_priority_id) => Sequel.qualify(:enumvals_processing_priority, :id),
+                      },
+                      {
+                        :table_alias => :enumvals_processing_priority
+                      }).
       select(
       Sequel.qualify(:accession, :id),
       Sequel.qualify(:accession, :identifier),
@@ -83,10 +134,15 @@ class WorkPlanDevelopmentReport < AbstractReport
       Sequel.qualify(:collection_management, :cataloged_note),
       Sequel.qualify(:enumvals_acquisition_type, :value).as(:acquisition_type),
       Sequel.qualify(:extent, :number).as(:extent_number),
-      Sequel.qualify(:enumvals_extent_type, :value).as(:extent_type)
+      Sequel.qualify(:enumvals_extent_type, :value).as(:extent_type),
+      Sequel.qualify(:collection_management, :processing_plan).as(:processing_plan),
+      Sequel.qualify(:enumvals_processing_status, :value).as(:processing_status),
+      Sequel.qualify(:enumvals_processing_priority, :value).as(:processing_priority),
     )
 
     dataset = dataset.where(Sequel.qualify(:accession, :repo_id) => @repo_id) if @repo_id
+    dataset = dataset.where(Sequel.qualify(:enumvals_processing_status, :value) => @processing_status) if @processing_status
+    dataset = dataset.where(Sequel.qualify(:enumvals_processing_priority, :value) => @processing_priority) if @processing_priority
 
     dataset.distinct(:id).order_by(Sequel.asc(:identifier), Sequel.asc(:title))
   end
