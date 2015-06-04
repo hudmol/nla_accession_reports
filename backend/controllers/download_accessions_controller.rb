26cb572730dcb4e7adc100a4d5a544357bde2701
jambun <jambun@gmail.com>
2015-06-03 21:41:51 -0400

    paginate the search

--- backend/controllers/download_accessions_controller.rb
+++ backend/controllers/download_accessions_controller.rb
@@ -21,7 +21,10 @@ class ArchivesSpaceService < Sinatra::Base
 
 
   def some_headers
-    {"Content-Type" => "text/csv; charset=UTF-8", "Content-Disposition" => "attachment; filename=\"accessions_#{Time.now.iso8601}.csv\""}
+    {
+      "Content-Type" => "text/csv; charset=UTF-8",
+      "Content-Disposition" => "attachment; filename=\"accessions_#{Time.now.iso8601}.csv\""
+    }
   end
 
 
@@ -29,33 +32,43 @@ class ArchivesSpaceService < Sinatra::Base
     params[:filter_term] ||= []
     params[:filter_term] << {'primary_type' => 'accession'}.to_json
     params[:page] = 1
-    params[:page_size] = 10000
-    resp = Search.search(params, params[:repo_id])
+    params[:page_size] = 100
 
     CSV.generate do |csv|
       csv << column_headings
-      resp['results'].each do |r|
-        j = ASUtils.json_parse(r['json'])
-        ud = j['user_defined'] || {}
-        extent = j['extents'].first || {}
-        csv << [
-                r['title'],
-                r['identifier'],
-                r['accession_date'],
-                j['content_description'],
-                j['inventory'],
-                j['retention_rule'],
-                j['access_restrictions_note'],
-                format_dates(j['dates']),
-                extent['number'],
-                extent['extent_type'],
-                extent['container_summary'],
-                format_subjects(r['subjects']),
-                ud['text_2'],
-                ud['text_3'],
-                ud['text_4'],
-                ud['enum_2'],
-               ]
+
+      while true
+        resp = Search.search(params, params[:repo_id])
+
+        resp['results'].each do |r|
+          j = ASUtils.json_parse(r['json'])
+          ud = j['user_defined'] || {}
+          extent = j['extents'].first || {}
+          csv << [
+                  r['title'],
+                  r['identifier'],
+                  r['accession_date'],
+                  j['content_description'],
+                  j['inventory'],
+                  j['retention_rule'],
+                  j['access_restrictions_note'],
+                  format_dates(j['dates']),
+                  extent['number'],
+                  extent['extent_type'],
+                  extent['container_summary'],
+                  format_subjects(r['subjects']),
+                  ud['text_2'],
+                  ud['text_3'],
+                  ud['text_4'],
+                  ud['enum_2'],
+                 ]
+        end
+
+        if params[:page] < resp['last_page']
+          params[:page] += 1
+        else
+          break
+        end
       end
     end
   end
