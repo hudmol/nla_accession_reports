fed9df53f4fb2435d8b5cef9acaddc63983ebe23
jambun <jambun@gmail.com>
2016-03-15 14:18:57 +1100

    added events - this will affect performance

--- backend/model/work_plan_development_report.rb
+++ backend/model/work_plan_development_report.rb
@@ -28,7 +28,10 @@ class WorkPlanDevelopmentReport < AbstractReport
     ['Accession No', 'Title', 'Arrival Date', 'Extent', "Container Summary", "Inventory", "Acq Method", 'Disposition',
      "Processing Status", "Processing Plan", "Processing Notes", "Processors",
      "Accessioning Priority", "Valuation Status", 'New Collection?', 'PO/Holdings Record', 'RefTracker No.',
-     'Digitisation Notes', 'Preservation Status']
+     'Digitisation Notes', 'Preservation Status',
+     'Processed Event Outcome', 'Catalogued Event Outcome', 'Accession Event Outcome', 'Registration Event Outcome',
+     'Acknowledgement Sent Event Outcome', 'Agreement Sent Event Outcome', 'Agreement Signed Event Outcome', 
+     'Publication Event Outcome', 'Ingestion Event Outcome']
   end
 
   def processor
@@ -70,6 +73,15 @@ class WorkPlanDevelopmentReport < AbstractReport
       'RefTracker No.' => proc{|record| record[:reftracker_no]},
       'Digitisation Notes' => proc{|record| record[:digitisation_notes]},
       'Preservation Status' => proc{|record| record[:preservation_status]},
+      'Processed Event Outcome' => proc{|record| record[:processed_outcome]},
+      'Catalogued Event Outcome' => proc{|record| record[:cataloged_outcome]},
+      'Accession Event Outcome' => proc{|record| record[:accession_outcome]},
+      'Registration Event Outcome' => proc{|record| record[:registration_outcome]},
+      'Acknowledgement Sent Event Outcome' => proc{|record| record[:acknowledgement_sent_outcome]},
+      'Agreement Sent Event Outcome' => proc{|record| record[:agreement_sent_outcome]},
+      'Agreement Signed Event Outcome' => proc{|record| record[:agreement_signed_outcome]},
+      'Publication Event Outcome' => proc{|record| record[:publication_outcome]},
+      'Ingestion Event Outcome' => proc{|record| record[:ingestion_outcome]},
     }
   end
 
@@ -79,10 +91,98 @@ class WorkPlanDevelopmentReport < AbstractReport
   end
 
   def query(db)
+
+    # save a bunch of joins by caching the ids for event_types
+    event_types = {}
+    db[:enumeration_value].
+      join(:enumeration,
+           {
+             :id => Sequel.qualify(:enumeration_value, :enumeration_id),
+             :name => "event_event_type"
+           }).
+      select(
+             Sequel.qualify(:enumeration_value, :id),
+             Sequel.qualify(:enumeration_value, :value)).
+      all{|row| event_types[row[:value]] = row[:id]}
+
     dataset = db[:accession].
       left_outer_join(:user_defined, :accession_id =>  Sequel.qualify(:accession, :id)).
       left_outer_join(:collection_management, :accession_id => Sequel.qualify(:accession, :id)).
       left_outer_join(:extent, :accession_id => Sequel.qualify(:accession, :id)).
+
+      left_outer_join(:event,
+           {
+             :id => (db[:event_link_rlshp].where(:accession_id => Sequel.qualify(:accession,:id)).select(:event_id)),
+             :event_type_id => event_types['processed']
+           },
+           {
+             :table_alias => :event_processed
+           }).
+      left_outer_join(:event,
+           {
+             :id => (db[:event_link_rlshp].where(:accession_id => Sequel.qualify(:accession, :id)).select(:event_id)),
+             :event_type_id => event_types['cataloged']
+           },
+           {
+             :table_alias => :event_cataloged
+           }).
+      left_outer_join(:event,
+           {
+             :id => (db[:event_link_rlshp].where(:accession_id => Sequel.qualify(:accession, :id)).select(:event_id)),
+             :event_type_id => event_types['accession']
+           },
+           {
+             :table_alias => :event_accession
+           }).
+      left_outer_join(:event,
+           {
+             :id => (db[:event_link_rlshp].where(:accession_id => Sequel.qualify(:accession, :id)).select(:event_id)),
+             :event_type_id => event_types['registration']
+           },
+           {
+             :table_alias => :event_registration
+           }).
+      left_outer_join(:event,
+           {
+             :id => (db[:event_link_rlshp].where(:accession_id => Sequel.qualify(:accession, :id)).select(:event_id)),
+             :event_type_id => event_types['acknowledgement_sent']
+           },
+           {
+             :table_alias => :event_acknowledgement_sent
+           }).
+      left_outer_join(:event,
+           {
+             :id => (db[:event_link_rlshp].where(:accession_id => Sequel.qualify(:accession, :id)).select(:event_id)),
+             :event_type_id => event_types['agreement_sent']
+           },
+           {
+             :table_alias => :event_agreement_sent
+           }).
+      left_outer_join(:event,
+           {
+             :id => (db[:event_link_rlshp].where(:accession_id => Sequel.qualify(:accession, :id)).select(:event_id)),
+             :event_type_id => event_types['agreement_signed']
+           },
+           {
+             :table_alias => :event_agreement_signed
+           }).
+      left_outer_join(:event,
+           {
+             :id => (db[:event_link_rlshp].where(:accession_id => Sequel.qualify(:accession, :id)).select(:event_id)),
+             :event_type_id => event_types['publication']
+           },
+           {
+             :table_alias => :event_publication
+           }).
+      left_outer_join(:event,
+           {
+             :id => (db[:event_link_rlshp].where(:accession_id => Sequel.qualify(:accession, :id)).select(:event_id)),
+             :event_type_id => event_types['ingestion']
+           },
+           {
+             :table_alias => :event_ingestion
+           }).
+
       join(:enumeration,
            {
              :name => 'accession_acquisition_type'
@@ -124,6 +224,13 @@ class WorkPlanDevelopmentReport < AbstractReport
            },
            {
              :table_alias => :enum_preservation_status
+           }).
+      join(:enumeration,
+           {
+             :name => 'event_outcome'
+           },
+           {
+             :table_alias => :enum_event_outcome
            }).
       left_outer_join(:enumeration_value,
                       {
@@ -173,6 +280,80 @@ class WorkPlanDevelopmentReport < AbstractReport
                       {
                         :table_alias => :enumvals_preservation_status
                       }).
+
+      left_outer_join(:enumeration_value,
+                      {
+                        Sequel.qualify(:enumvals_event_processed_outcome, :enumeration_id) =>  Sequel.qualify(:enum_event_outcome, :id),
+                        Sequel.qualify(:event_processed, :outcome_id) => Sequel.qualify(:enumvals_event_processed_outcome, :id),
+                      },
+                      {
+                        :table_alias => :enumvals_event_processed_outcome
+                      }).
+      left_outer_join(:enumeration_value,
+                      {
+                        Sequel.qualify(:enumvals_event_cataloged_outcome, :enumeration_id) =>  Sequel.qualify(:enum_event_outcome, :id),
+                        Sequel.qualify(:event_cataloged, :outcome_id) => Sequel.qualify(:enumvals_event_cataloged_outcome, :id),
+                      },
+                      {
+                        :table_alias => :enumvals_event_cataloged_outcome
+                      }).
+      left_outer_join(:enumeration_value,
+                      {
+                        Sequel.qualify(:enumvals_event_accession_outcome, :enumeration_id) =>  Sequel.qualify(:enum_event_outcome, :id),
+                        Sequel.qualify(:event_accession, :outcome_id) => Sequel.qualify(:enumvals_event_accession_outcome, :id),
+                      },
+                      {
+                        :table_alias => :enumvals_event_accession_outcome
+                      }).
+      left_outer_join(:enumeration_value,
+                      {
+                        Sequel.qualify(:enumvals_event_registration_outcome, :enumeration_id) =>  Sequel.qualify(:enum_event_outcome, :id),
+                        Sequel.qualify(:event_registration, :outcome_id) => Sequel.qualify(:enumvals_event_registration_outcome, :id),
+                      },
+                      {
+                        :table_alias => :enumvals_event_registration_outcome
+                      }).
+      left_outer_join(:enumeration_value,
+                      {
+                        Sequel.qualify(:enumvals_event_acknowledgement_sent_outcome, :enumeration_id) =>  Sequel.qualify(:enum_event_outcome, :id),
+                        Sequel.qualify(:event_acknowledgement_sent, :outcome_id) => Sequel.qualify(:enumvals_event_acknowledgement_sent_outcome, :id),
+                      },
+                      {
+                        :table_alias => :enumvals_event_acknowledgement_sent_outcome
+                      }).
+      left_outer_join(:enumeration_value,
+                      {
+                        Sequel.qualify(:enumvals_event_agreement_sent_outcome, :enumeration_id) =>  Sequel.qualify(:enum_event_outcome, :id),
+                        Sequel.qualify(:event_agreement_sent, :outcome_id) => Sequel.qualify(:enumvals_event_agreement_sent_outcome, :id),
+                      },
+                      {
+                        :table_alias => :enumvals_event_agreement_sent_outcome
+                      }).
+      left_outer_join(:enumeration_value,
+                      {
+                        Sequel.qualify(:enumvals_event_agreement_signed_outcome, :enumeration_id) =>  Sequel.qualify(:enum_event_outcome, :id),
+                        Sequel.qualify(:event_agreement_signed, :outcome_id) => Sequel.qualify(:enumvals_event_agreement_signed_outcome, :id),
+                      },
+                      {
+                        :table_alias => :enumvals_event_agreement_signed_outcome
+                      }).
+      left_outer_join(:enumeration_value,
+                      {
+                        Sequel.qualify(:enumvals_event_publication_outcome, :enumeration_id) =>  Sequel.qualify(:enum_event_outcome, :id),
+                        Sequel.qualify(:event_publication, :outcome_id) => Sequel.qualify(:enumvals_event_publication_outcome, :id),
+                      },
+                      {
+                        :table_alias => :enumvals_event_publication_outcome
+                      }).
+      left_outer_join(:enumeration_value,
+                      {
+                        Sequel.qualify(:enumvals_event_ingestion_outcome, :enumeration_id) =>  Sequel.qualify(:enum_event_outcome, :id),
+                        Sequel.qualify(:event_ingestion, :outcome_id) => Sequel.qualify(:enumvals_event_ingestion_outcome, :id),
+                      },
+                      {
+                        :table_alias => :enumvals_event_ingestion_outcome
+                      }).
+
       select(
       Sequel.qualify(:accession, :id),
       Sequel.qualify(:accession, :identifier),
@@ -196,6 +377,15 @@ class WorkPlanDevelopmentReport < AbstractReport
       Sequel.qualify(:user_defined, :string_2).as(:reftracker_no),
       Sequel.qualify(:user_defined, :text_5).as(:digitisation_notes),
       Sequel.qualify(:enumvals_preservation_status, :value).as(:preservation_status),
+      Sequel.qualify(:enumvals_event_processed_outcome, :value).as(:processed_outcome),
+      Sequel.qualify(:enumvals_event_cataloged_outcome, :value).as(:cataloged_outcome),
+      Sequel.qualify(:enumvals_event_accession_outcome, :value).as(:accession_outcome),
+      Sequel.qualify(:enumvals_event_registration_outcome, :value).as(:registration_outcome),
+      Sequel.qualify(:enumvals_event_acknowledgement_sent_outcome, :value).as(:acknowledgement_sent_outcome),
+      Sequel.qualify(:enumvals_event_agreement_sent_outcome, :value).as(:agreement_sent_outcome),
+      Sequel.qualify(:enumvals_event_agreement_signed_outcome, :value).as(:agreement_signed_outcome),
+      Sequel.qualify(:enumvals_event_publication_outcome, :value).as(:publication_outcome),
+      Sequel.qualify(:enumvals_event_ingestion_outcome, :value).as(:ingestion_outcome),
     )
 
     dataset = dataset.where(Sequel.qualify(:accession, :repo_id) => @repo_id) if @repo_id
